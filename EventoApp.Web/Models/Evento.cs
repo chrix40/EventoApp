42076e2117b911b0d5849a834262c957b4950404
Christian Roberto <christian2020022014@gmail.com>
2022-04-28 10:06:42 -0400

    wip

---
+++ EventoApp.Web/Models/Evento.cs
@@ -0,0 +1,12 @@
+﻿namespace EventoApp.Web.Models
+{
+    public class Evento
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? preco { get; set; }
+        public string? Image { get; set; }
+        public string? Data { get; set; }
+    }
+}
